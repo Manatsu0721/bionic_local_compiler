0504c09d175bffdd10596d8674705e8158a2419c
Konoka <bwrp17001499@outlook.com>
2026-07-27 11:47:15 +0800

    更新 run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ export NDK=$HOME/android-ndk-r15c
 export SYSROOT=$NDK/platforms/android-24/arch-arm64
 
 export DEP_ROOT=$HOME/project/deps
-export GCC_SRC=$HOME/Desktop/gcc-4.9-src
+export GCC_SRC=$HOME/project/gcc-4.9-src
 export BUILD_DIR=$HOME/project/gcc-build2
 export PATH=~/project/gcc-wrappers:$NDK/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
 export CC=aarch64-linux-android-gcc

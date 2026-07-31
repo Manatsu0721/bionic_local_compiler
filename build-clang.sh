dc0a9865fccb03e37c934c6a73669cccc008d39c
Ramdisk01 <bwrp17001499@outlook.com>
2026-07-31 22:40:47 +0800

    strip package

--- build-clang.sh
+++ build-clang.sh
@@ -10,13 +10,25 @@ cmake -G Ninja ../llvm \
     -DCMAKE_BUILD_TYPE=Release \
     -DLLVM_ENABLE_PROJECTS="clang;lld" \
     -DLLVM_TARGETS_TO_BUILD="AArch64;ARM;X86" \
+    -DLLVM_DEFAULT_TARGET_TRIPLE="aarch64-linux-android" \
     -DLLVM_EXTERNAL_CLANG_SOURCE_DIR=../clang \
     -DLLVM_EXTERNAL_LLD_SOURCE_DIR=../lld \
     -DCMAKE_INSTALL_PREFIX=$(pwd)/install \
     -DLLVM_ENABLE_ZLIB=OFF \
     -DLLVM_ENABLE_TERMINFO=OFF \
     -DLLVM_ENABLE_LIBXML2=OFF \
-    -DLLVM_NATIVE_CMAKE_FLAGS="-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++" 
+    -DLLVM_NATIVE_CMAKE_FLAGS="-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++" \
+    -DLLVM_VERSION_MAJOR=9 \
+    -DLLVM_VERSION_MINOR=0 \
+    -DLLVM_VERSION_PATCH=3 \
+    -DCLANG_VENDOR="Android (5484270 based on r353983c) " \
+    -DCLANG_REPOSITORY_STRING="https://android.googlesource.com/toolchain/clang" \
+    -DCLANG_REVISION="745b335211bb9eadfa6aa6301f84715cee4b37c5" \
+    -DLLVM_REPOSITORY_STRING="https://android.googlesource.com/toolchain/llvm" \
+    -DLLVM_REVISION="60cf23e54e46c807513f7a36d0a7b777920b5881" \
+    -DLLVM_VERSION_SUFFIX="svn" 
+      
+
 
 ninja -j$(nproc)
-ninja install
+ninja install-distribution-stripped
